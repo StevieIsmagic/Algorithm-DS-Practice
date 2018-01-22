c33d5c31a2c3b9f6f748815e0d2c5bee80729b33
Colombianito66 <colombianito66@gmail.com>
2018-01-22 18:06:45 -0500

    ad idem

--- notes2.js
+++ notes2.js
@@ -23,10 +23,48 @@ const newUser2 = new User({
 
 console.log(newUser2, newUser2.verifyPassword('asdf')); // true
 
-/* if 'this' is not bound to anything, it refers to the global object */
+/* 2. if 'this' is not bound to anything, it refers to the global object */
 
 function foo() {
   console.log(this);
 }
 
 foo() // will log the global object
+
+// 3. manually set the 'this' object reference
+foo.call({ message: 'hello world' }, true, null, 'hello');
+foo.apply({ message: 'hello world' }, [true, null, 'hello']);
+const boundFoo = foo.bind({ message: 'hello world' });
+
+/* 4. when 'new' keyword is being used, 'this' refers to the new object being created */
+
+const user = new User3({
+  email: 'asd@gmail.com',
+  password: 'asd'
+});
+
+console.log(user);
+
+// a static method is one that attaches the function to the class, not the instance of the class
+
+class User {
+	constructor({ email, password }) {
+    this.email = email;
+    this.password = password;
+  }
+  
+  static sayHi() {
+    console.log('hi');
+  }
+
+}
+// will need to invoke using capital U- User
+User.sayHi();
+
+
+
+
+
+
+
+
