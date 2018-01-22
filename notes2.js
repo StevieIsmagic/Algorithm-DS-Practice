2dfe9fdb2be6a19c2fa9f268a2c2a0c67d1dc06b
Colombianito66 <colombianito66@gmail.com>
2018-01-22 17:53:58 -0500

    ad idem

--- notes2.js
+++ notes2.js
@@ -2,12 +2,23 @@ class User {
 	constructor({ email, password }) {
     this.email = email;
     this.password = password;
-	}
+  }
+  
+  verifyPassword(passwordToCheck) {
+    return passwordToCheck === this.password;
+  }
 }
 
 const newUser = new User({
 	email: 'asdf@gmail.com',
 	password: 'asdf'
-})
+});
 
-console.log(newUser);
+console.log(newUser, newUser.verifyPassword('qwerty')); // false
+
+const newUser2 = new User({
+  email: 'addsf',
+  password: 'asdf'
+});
+
+console.log(newUser2, newUser2.verifyPassword('asdf')); // true
