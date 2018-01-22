c0253790e73b19713dd9cbad53d204b9df46cf0d
Colombianito66 <colombianito66@gmail.com>
2018-01-22 17:51:49 -0500

    ad idem

--- notes2.js
+++ notes2.js
@@ -1,11 +1,13 @@
 class User {
-	constructor(props) {
-		super(props);
+	constructor({ email, password }) {
+    this.email = email;
+    this.password = password;
 	}
-
 }
 
 const newUser = new User({
 	email: 'asdf@gmail.com',
 	password: 'asdf'
 })
+
+console.log(newUser);
