7b2eb360f29a7016b2bb2c10d98bab4e0d259222
Colombianito66 <colombianito66@gmail.com>
2018-02-06 23:48:55 -0500

    Constructor Pattern

--- Object-creation-patterns.js
+++ Object-creation-patterns.js
@@ -1,4 +1,4 @@
-/* ***********   PRE-ES-6 ***********/
+/* ***********  OBJECT CREATION PRE-ES6 ***********/
 
 /* Factory Pattern */
 
@@ -23,5 +23,30 @@ person1.printPerson();
 person2.printPerson();
 
 /* Constructor Pattern */
+
+var peopleConstructor = function(name, age, state) {
+  this.name = name;
+  this.age = age;
+  this.state = state;
+
+  this.printPerson = function() {
+    console.log(this.name + ', ' + this.age + ', ' + this.state);
+  };
+};
+
+// unlike factory pattern, since we arent returning any object
+// we have to create an object from the peopleConstructor object itself
+// so we must use a Constructor...this behaves like a class constructor
+// similar to Java + ES6
+
+var person1 = new peopleConstructor('Stevie', 27, 'CA');
+var person2 = new peopleConstructor('Karen', 29, 'FL');
+
+person1.printPerson();
+person2.printPerson();
+// the problem with this is that each time a person object is created,
+// it carries all of the properties containted in its conctructor
+// AKA each will have its own printPerson method.
+// Prototype pattern helps reduce this overhead
 
 /* Prototype Pattern */
