2e9cf41565891e727f853099293d000b2bd0284e
Colombianito66 <colombianito66@gmail.com>
2018-02-12 18:37:41 -0500

    Big O analysis

--- Big-O/QuadraticVsLinear.js
+++ Big-O/QuadraticVsLinear.js
@@ -2,12 +2,17 @@
 
 // for N elements in the array, how many steps would our Alg take in a worst case scenario?
 function hasDuplicateValue(array) {
+  let steps = 0; // use this to compare # of steps
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length; j++) {
+      steps++;
       if (i !== j && array[i] === array[j]) {
         return true;
       }
     }
   }
+  console.log(steps);
   return false;
 }
+
+// Linear Solution O(n)
