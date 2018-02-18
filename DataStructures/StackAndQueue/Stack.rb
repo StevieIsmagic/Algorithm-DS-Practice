4e3e1a060f5889477734719c573b9331e4b0b34b
Colombianito66 <colombianito66@gmail.com>
2018-02-18 11:22:56 -0500

    Stacks + Queues

---
+++ DataStructures/StackAndQueue/Stack.rb
@@ -0,0 +1,14 @@
+=begin 
+Stacks (LIFO) and Queues (FIFO) are simply arrays with restrictions.
+They are useful for handling temporary data, from 
+operating system architecture, to printing jobs,
+to traversing data, stacks and queues serve as temporary 
+containers that can be used to form beautiful algorithms
+
+STACK CONSTRAINTS:
+- Data can only be Inserted at the end of stack
+- Data can only be Read from the end of stack
+- Data can only be Removed from the end of stack
+
+
+=end
