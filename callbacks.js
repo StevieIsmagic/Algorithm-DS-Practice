313130914fc4eed7eeaaf843e817b4fc34d0e32b
Colombianito66 <colombianito66@gmail.com>
2018-02-25 15:08:20 -0500

    callbacks practice

--- callbacks.js
+++ callbacks.js
@@ -32,3 +32,43 @@ properties (or global properties) as a parameter to the cb f(x). In the
 preceding example, we pass `options` as a parameter to the cb f(x).
 Let's pass a global variable and a local variable:
 */
+
+//Global variable
+var generalLastName = 'Magic';
+
+function getInput(options, callback) {
+  allUserData.push(options);
+  //          global var    local var
+  callback(generalLastName, options);
+}
+
+/* MAKE SURE CB() IS A F(x) BEFORE CALLING IT 
+It is always wise to check that teh cb f(x) passed in the parameter
+is indeed a function before calling it. Also, it is good practice to make the 
+cb f(x) optional. Let's refactor the `getInput` function from the previous
+example to ensure these checks are in place. 
+*/
+
+function getInput(options, callback) {
+  allUserData.push(options);
+
+  //Make sure the callback is a function
+  if (typeof callback === 'function') {
+    // Call it, since we have confirmed it is callable
+    callback(options);
+  }
+}
+
+/* Without the check in place, if the `getInput` function is called either
+without the callback function as a parameter or in place of a function
+a non-function is passed, our code will result in a runtime error.
+*/
+
+/* PROBLEM WHEN USING METHODS WITH THE `this` OBJECT AS CALLBACKS
+When the callback function is a method that uses the `this` object, we have
+to modify how we execute the callback function to preserve the `this` object
+context. Or else the `this` object will EITHER (1) point to the global window object
+(in the browser), if cb was passed to a global function (2) Or it will point to
+the object of the containing method.
+Let's explore this in code:
+*/
