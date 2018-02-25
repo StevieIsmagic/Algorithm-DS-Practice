fe2dac1404d1a77ace26dfbb01aef7fe0192a944
Colombianito66 <colombianito66@gmail.com>
2018-02-25 14:38:41 -0500

    callbacks practice

--- callbacks.js
+++ callbacks.js
@@ -24,3 +24,11 @@ function getInput(options, callback) {
 getInput({ name: 'Stevie', specialty: 'Eating cheetos' }, logStuff);
 // name: Stevie
 // specialty: Eating Cheetos
+
+/* *** PASS PARAMETERS TO CALLBACK F(x)s *** 
+Since the callback function is just a normal function when it is executed,
+we can pass parameters to it. We can pass any of the containing function's 
+properties (or global properties) as a parameter to the cb f(x). In the 
+preceding example, we pass `options` as a parameter to the cb f(x).
+Let's pass a global variable and a local variable:
+*/
