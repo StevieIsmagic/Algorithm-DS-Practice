80a44db8dc114e0f3a9f777313d9d490c5b8268a
Colombianito66 <colombianito66@gmail.com>
2018-03-04 12:36:57 -0500

    fix bug

--- notes.js
+++ notes.js
@@ -1,21 +1,21 @@
-function factorial(n){
-if(n < 0 || n > 12) {
-throw new RangeError('Parameter must be between ' + '0' + ' and ' + '12');
-} if(n === 0) {
-return 1;
-} 
-  return n * factorial(n -1);
+function factorial(n) {
+  if (n < 0 || n > 12) {
+    throw new RangeError('Parameter must be between ' + '0' + ' and ' + '12');
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
-********************************
+/******************************* */
 
-function factorial(n)
-{
-    if(n < 0 || n > 12) throw new RangeError('Ошибка');
-    return n ? n * factorial(n-1) : 1;
+function factorial(n) {
+  if (n < 0 || n > 12) throw new RangeError('Ошибка');
+  return n ? n * factorial(n - 1) : 1;
 }
 
-***************** LINKED LIST ******************
+/* **************** LINKED LIST ***************** 
 (1) Node
 	- data
 	- next
@@ -28,32 +28,35 @@ function factorial(n)
 	- add()
 	- searchNodeAt()
 	- remove()
+*/
 
 function node(data) {
-	this.data = data;
-	this.next = null;
+  this.data = data;
+  this.next = null;
 }
 
 function linkedList() {
-	this.length = 0;
-	this.head = null;
+  this.length = 0;
+  this.head = null;
 }
 
 // ADD NODE TO LIST
 linkedList.prototype.add = function(value) {
-	var node = new node(value);
-	currentNode = this.head;
+  var node = new node(value);
+  currentNode = this.head;
 
-	if (!currentNode) {  // Empty List
-		this.head = node;
-		this.length++;
-		return node;
-	}
+  if (!currentNode) {
+    // Empty List
+    this.head = node;
+    this.length++;
+    return node;
+  }
 
-	while (currentNode.next) {  //  Non-Empty List 
-		currentNode = currentNode.next;
-	}
-	currentNode.next = node;
-	this.length++;
-	return node;
-}
+  while (currentNode.next) {
+    //  Non-Empty List
+    currentNode = currentNode.next;
+  }
+  currentNode.next = node;
+  this.length++;
+  return node;
+};
